9c6c77d15c63f17763b965f47b5d580d7bd5c31a
Nick Rabinowitz <nick@unfolded.ai>
2021-02-20 18:10:15 -0800

    Fix prettier types

--- types.d.ts
+++ types.d.ts
@@ -8,19 +8,19 @@ declare module 'geojson2h3' {
      * shape, at a level of precision determined by the hexagon resolution.
      */
     export function featureToH3Set(feature: Feature, resolution: number): string[];
-    
+
     /**
      * Convert a single H3 hexagon to a GeoJSON `Polygon` feature
      */
     export function h3ToFeature(h3Index: string, properties: GeoJsonProperties): Feature;
-    
+
     /**
      * Convert a set of hexagons to a GeoJSON `Feature` with the set outline(s). The
      * feature's geometry type will be either `Polygon` or `MultiPolygon` depending on
      * the number of outlines required for the set.
      */
     export function h3SetToFeature(hexagons: string[], properties: GeoJsonProperties): Feature;
-    
+
     /**
      * Convert a set of hexagons to a GeoJSON `MultiPolygon` feature with the
      * outlines of each individual hexagon.
